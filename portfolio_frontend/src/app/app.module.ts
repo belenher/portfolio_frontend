ba8c018e2ea16eafb0539681e27c9984b5b602d6
belenher <101005436+belenher@users.noreply.github.com>
2022-09-16 16:22:36 -0300

    Se crean componentes experiencia y educación

--- portfolio_frontend/src/app/app.module.ts
+++ portfolio_frontend/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoAPComponent,
     HeaderComponent,
     AcercaDeComponent,
-    BannerComponent
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
